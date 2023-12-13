096327fcb479a7b66f94e2fe61d1614bfe8b254c
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-12-13 14:48:13 +0000

    Update day4part1.py

--- Day4/day4part1.py
+++ Day4/day4part1.py
@@ -25,6 +25,28 @@ with open("puzzle_input.txt") as f:
 
         score += indiv_point
 
+    score2 = 0
+
+    mult = [1 for i in file]
+
+    for i, line in enumerate(file):
+
+        user_nums = [int(u) for u in line.split(":")[1].split("|")[1].split()]
+        winning_nums = [int(w) for w in line.split(":")[1].split("|")[0].split()]
+
+        matching_nums = [m for m in user_nums if m in winning_nums]
+
+        
+        c_mult = mult[i]
+
+        for j in range(i + 1, min(i + len(matching_nums) + 1, len(file))):
+
+            mult[j] += c_mult
+
+        score2 += c_mult
+
             
 
-    print(score)
+    print(f"Part 1 Solution: {score}")
+
+    print(f"Part 2 Solution {score2}")
